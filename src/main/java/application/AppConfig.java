55d60628b13fd16d8d072423041c1c75f60ccee7
VitaliyMi <miroshnichenkovitaliy@gmail.com>
2016-06-13 22:27:46 -0400

    Repository not working

--- src/main/java/application/AppConfig.java
+++ src/main/java/application/AppConfig.java
@@ -3,9 +3,13 @@ package application;
 import data.DAOClass;
 import data.DAOHibernateImpl;
 import data.dishaccess.DishesRepository;
+import jdk.management.resource.internal.inst.DatagramDispatcherRMHooks;
+import model.entities.Dish;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -24,18 +28,20 @@ import java.util.Properties;
  * Created by MSI on 30.04.2016.
  */
 
+
 @Configuration
 @ComponentScan("java")
-//@EnableJpaRepositories(basePackageClasses=DishesRepository.class)
+@EnableJpaRepositories("data")
+@EntityScan("model.entities")
 @EnableAutoConfiguration
 public class AppConfig {
 
     @Bean
     public EntityManager entityManager()
-   {
-       EntityManagerFactory emf = Persistence.createEntityManagerFactory("MarketOnline");
-       return emf.createEntityManager();
-   }
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MarketOnline");
+        return emf.createEntityManager();
+    }
 
     @Bean
     public DAOClass daoClass()
@@ -43,4 +49,54 @@ public class AppConfig {
         return new DAOHibernateImpl();
     }
 
+   /* @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("MarketOnline");
+        return emf;
+    }
+
+   @Bean(destroyMethod = "close")
+   public DataSource dataSource() {
+       BasicDataSource basicDataSource = new BasicDataSource();
+       basicDataSource.setDriverClassName("org.h2.Driver");
+       basicDataSource.setUrl("jdbc:h2:mem:testdb");
+       basicDataSource.setUsername("sa");
+       basicDataSource.setPassword("");
+       return (DataSource) basicDataSource;
+   }
+
+    /*
+     * Factory to create a especific instance of Entity Manager
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean entityManager = new LocalContainerEntityManagerFactoryBean();
+        entityManager.setDataSource(dataSource());
+        entityManager.setPackagesToScan("data");
+        entityManager.setPersistenceProvider(new HibernatePersistenceProvider());
+        entityManager.setJpaProperties(jpaProperties());
+
+        return entityManager;
+    }
+
+    /**
+     * Properties for Jpa
+
+    private static Properties jpaProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        properties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+        return properties;
+    }
+
+    /**
+     * Get transaction manager
+
+    @Bean
+    public JpaTransactionManager transactionManager() throws SQLException {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return transactionManager;
+    }
+     */
 }
