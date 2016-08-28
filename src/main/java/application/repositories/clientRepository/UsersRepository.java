9bcce754b131040751a56b17ce74ea5c9ebf0c38
VitaliyMi <miroshnichenkovitaliy@gmail.com>
2016-08-28 11:55:41 -0400

    Sonarqube

--- src/main/java/application/repositories/clientRepository/UsersRepository.java
+++ src/main/java/application/repositories/clientRepository/UsersRepository.java
@@ -6,7 +6,6 @@ import application.model.entities.Client;
  * Created by MSI on 05.06.2016.
  */
 
-//@Repository
 public interface UsersRepository// extends CrudRepository<Client, Integer>, JpaSpecificationExecutor<Client>
  {
      Client findByName(String userName);
